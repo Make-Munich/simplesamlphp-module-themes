4e26c73e445a35b973f6fa80f448d31f3fe90940
derchris <derchris@me.com>
2018-10-05 18:20:16 +0200

    modify layout

--- themes/modern/default/includes/header.php
+++ themes/modern/default/includes/header.php
@@ -80,7 +80,7 @@ endif;
 					</div>
 					<div class="collapse navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="/module.php/selfregister/reviewUser.php">Login</li>
+              <li><a href="/module.php/selfregister/reviewUser.php">Login</a></li>
 						  <li>
 								<form class="navbar-form navbar-left" action="#" method="post">
 									<select class="selectpicker form-control" data-width="fit" name="language" onchange='this.form.submit()'>
